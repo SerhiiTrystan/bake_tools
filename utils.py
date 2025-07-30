d3f0b01a04e11ba1b98d065ad9aa2598513e7cfe
SerhiiTrystan <salingartist@gmail.com>
2025-07-30 23:01:38 +0200

    try to make system

--- utils.py
+++ utils.py
@@ -16,7 +16,7 @@ import bpy
     links.new(transform.outputs["Geometry"], output.inputs["Geometry"])
 
     return node_group
-
+#________________________________
     def create_geo_node_group(group_name="MainNG"):
     #добавление группы нодов
         if group_name in bpy.data.node_groups:
@@ -24,7 +24,7 @@ import bpy
 
         node_group = bpy.data.node_groups.new(group_name, 'GeometryNodeTree')
 
-    #чистим сокеты
+        #чистим сокеты
         while node_group.inputs:
             node_group.inputs.romove(node_group.inputs[0])
         while node_group.outputs:
@@ -36,13 +36,29 @@ import bpy
         links = node_group.links
         nodes.clear()
 
-        #создаем систему узлов
+    #создаем систему узлов
 
         inp_nd = nodes.new(type='NodeGroupInput')
-        inp_nd.lacation = (,)
+        inp_nd.lacation = (620,300)
         
         out_nd = nodes.new(type='NodeGroupOutput')
-        out_nd.location = (,)
+        out_nd.location = (660,350)
+        out_nd.interface_item_new(item_type='INPUT')
 
         nm_attrb = nodes.new(type='GeometryNodeStoreNamedAttribute')
-        nm_attrb.location = (,)
+        nm_attrb.location = (530,100)
+
+        rgb_curve = nodes.new(type='ShaderNodeRGBCurve')
+        rgb_curve.location = (700,300)
+
+        cl_rmp = nodes.new(type='ShaderNodeValToRGB')
+        cl_rmp.location = (950,320)
+
+        blur_attrb = nodes.new(type='GeometryNodeBlurAttribute')
+        blur_attrb.location = (730,250)
+    #делаем связи 
+        links.new(inp_nd.outputs["Geometry"], out_nd.iputs["Geometry"])
+
+
+
+        return node_group
